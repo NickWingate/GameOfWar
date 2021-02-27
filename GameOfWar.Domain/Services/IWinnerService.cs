4202608fcfa548cd0dc24dc4e4da1a1cadc5dee4
NickWingate <nicholas.wingate03@gmail.com>
2021-02-27 23:24:39 +0000

    Adds service to determine winner
    WinnerService.cs which implementes
    IWinnerService.cs

---
+++ GameOfWar.Domain/Services/IWinnerService.cs
@@ -0,0 +1,9 @@
+﻿using GameOfWar.Domain.Entities;
+
+namespace GameOfWar.Domain.Services
+{
+	public interface IWinnerService
+	{
+		public Player DetermineWinner(Player player1, Player player2);
+	}
+}
---
+++ GameOfWar.Domain/Services/WinnerService.cs
@@ -0,0 +1,31 @@
+﻿using GameOfWar.Domain.Entities;
+
+namespace GameOfWar.Domain.Services
+{
+	public class WinnerService : IWinnerService
+	{
+		public Player DetermineWinner(Player player1, Player player2)
+		{
+			if (player1.CurrentCard.Rank == player2.CurrentCard.Rank)
+			{
+				return null;
+			}
+
+			return CompareRanks(player1, player2);
+		}
+
+		private static Player CompareRanks(Player player1, Player player2)
+		{
+			var player1RankValue = (int) player1.CurrentCard.Rank;
+			var player2RankValue = (int) player2.CurrentCard.Rank;
+			return player1RankValue > player2RankValue ? player1 : player2;
+		}
+
+		private Player CompareSuits(Player player1, Player player2)
+		{
+			var player1SuitValue = (int)player1.CurrentCard.Suit;
+			var player2SuitValue = (int)player2.CurrentCard.Suit;
+			return player1SuitValue > player2SuitValue ? player1 : player2;
+		}
+	}
+}
