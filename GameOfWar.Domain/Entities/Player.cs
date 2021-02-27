c5a41d73548439f4a12196db3d3c0855cfe0c446
NickWingate <nicholas.wingate03@gmail.com>
2021-02-27 23:23:01 +0000

    Adds Player class

---
+++ GameOfWar.Domain/Entities/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfWar.Domain.Entities
+{
+	public class Player
+	{
+		public Player(string name)
+		{
+			Name = name;
+		}
+		public string Name { get; set; }
+		public List<Card> Hand { get; set; }
+		public Card CurrentCard { get; set; }
+		
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
